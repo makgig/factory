a731f01e2f5818a3d0a314668a5e7605cae31950
Ignatev Maksim <makgig@gmail.com>
2025-07-11 01:17:17 +0300

    fix for env int

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,9 @@ package config
 
 import (
 	"flag"
+	"fmt"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/caarlos0/env/v10"
@@ -35,12 +38,12 @@ func LoadServerConfig() (*Config, error) {
 func LoadAgentConfig() (*Config, error) {
 	cfg := &Config{
 		Address:        "localhost:8080",
-		ReportInterval: 10,
-		PollInterval:   2,
+		ReportInterval: 10 * time.Second,
+		PollInterval:   2 * time.Second,
 	}
 	address := flag.String("a", cfg.Address, "адрес эндпоинта HTTP-сервера")
-	reportInt := flag.Int("r", int(cfg.ReportInterval), "частота отправки метрик на сервер (секунды)")
-	pollInt := flag.Int("p", int(cfg.PollInterval), "частота опроса метрик из runtime (секунды)")
+	reportInt := flag.Int("r", int(cfg.ReportInterval.Seconds()), "частота отправки метрик на сервер (секунды)")
+	pollInt := flag.Int("p", int(cfg.PollInterval.Seconds()), "частота опроса метрик из runtime (секунды)")
 
 	if !flag.Parsed() {
 		flag.Parse()
@@ -50,8 +53,31 @@ func LoadAgentConfig() (*Config, error) {
 	cfg.ReportInterval = time.Duration(*reportInt) * time.Second
 	cfg.PollInterval = time.Duration(*pollInt) * time.Second
 
-	if err := env.Parse(cfg); err != nil {
-		return nil, err
+	// "github.com/caarlos0/env/v10"
+	// Все просто и готово к использованию, но ожидает формат time.Duration (5s) В тестах передается Int (5)
+
+	// if err := env.Parse(cfg); err != nil {
+	// 	return nil, err
+	// }
+
+	if addr := os.Getenv("ADDRESS"); addr != "" {
+		cfg.Address = addr
+	}
+
+	if reportStr := os.Getenv("REPORT_INTERVAL"); reportStr != "" {
+		if seconds, err := strconv.Atoi(reportStr); err != nil {
+			return nil, fmt.Errorf("REPORT_INTERVAL must be integer, got: %s", reportStr)
+		} else {
+			cfg.ReportInterval = time.Duration(seconds) * time.Second
+		}
+	}
+
+	if pollStr := os.Getenv("POLL_INTERVAL"); pollStr != "" {
+		if seconds, err := strconv.Atoi(pollStr); err != nil {
+			return nil, fmt.Errorf("POLL_INTERVAL must be integer, got: %s", pollStr)
+		} else {
+			cfg.PollInterval = time.Duration(seconds) * time.Second
+		}
 	}
 
 	return cfg, nil
